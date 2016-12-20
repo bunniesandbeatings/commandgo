2cb234a84a240dafc6365bf89f759ad9bbb6c84b
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2016-12-19 21:50:06 -0500

    Add BDD terms to ginkgo

---
+++ ginkgocumber/ginkgocumber.go
@@ -0,0 +1,15 @@
+package ginkgocumber
+
+import . "github.com/onsi/ginkgo"
+
+var (
+Given = Context
+FGiven = FContext
+XGiven = XContext
+When = Context
+FWhen = FContext
+XWhen = XContext
+Then = It
+FThen = FIt
+XThen = XIt
+)
