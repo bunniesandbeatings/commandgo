52c9de18ddc029ed71ed705b64529dc28342432a
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2016-12-19 21:51:44 -0500

    reformat for readability and human decency

--- ginkgocumber/ginkgocumber.go
+++ ginkgocumber/ginkgocumber.go
@@ -3,13 +3,15 @@ package ginkgocumber
 import . "github.com/onsi/ginkgo"
 
 var (
-Given = Context
-FGiven = FContext
-XGiven = XContext
-When = Context
-FWhen = FContext
-XWhen = XContext
-Then = It
-FThen = FIt
-XThen = XIt
+	Given  = Context
+	FGiven = FContext
+	XGiven = XContext
+
+	When   = Context
+	FWhen  = FContext
+	XWhen  = XContext
+
+	Then   = It
+	FThen  = FIt
+	XThen  = XIt
 )
